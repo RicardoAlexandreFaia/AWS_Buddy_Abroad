e55d1cfc7735e9a604e2388afb0bf95fa81cc0a0
Nuno Caetano <a21902813@alunos.ulht.pt>
2021-09-03 21:37:35 +0100

    get trips by id

--- buddyAbroadAPI/buddyAbroadAPI/urls.py
+++ buddyAbroadAPI/buddyAbroadAPI/urls.py
@@ -7,9 +7,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title = "API",
+        title = "Buddy Abroad API",
         default_version="v1",
-        description="A simple API",
+        description="The Backend of the app Buddy-Abroad",
         contact=openapi.Contact(email="BuddyAbroadDev@buddy.com")
     ),
     public=True,
@@ -20,6 +20,7 @@ urlpatterns = [
     path('confirmAccount/',Users.confirm_sign_up,name='confirm_sign_up'),
     path('login/',Users.login_auth,name='loginAuth'),
     path('trips/',TripsAPI.get,name='trips'),
+    path('getTrip/<int:id>',TripsAPI.get_trip_by_id),
     path('', views.api_root),
     path('swagger/',schema_view.with_ui('swagger',cache_timeout=0),name="schema-swagger-ui"),
 ]
